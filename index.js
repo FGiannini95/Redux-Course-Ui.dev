5c45d5ed1529f5d93770afa33f24fb2d976ad4c4
federico <fgiannini95@gmail.com>
2025-09-18 10:57:02 +0100

    Connecting with Ui

--- index.js
+++ index.js
@@ -129,7 +129,41 @@ const store = createStore(combineReducers);
 
 store.subscribe(() => {
   console.log("The new state is: ", store.getState());
+
+  const { todos, goals } = store.getState();
+  console.log("todos", todos);
+  console.log("goals", goals);
+
+  // Reset element in order to avoid duplication in the Ui
+  document.getElementById("goals").innerHTML = "";
+  document.getElementById("todos").innerHTML = "";
+
+  todos.forEach(addTodoToDom);
+  goals.forEach(addGoalToDOM);
 });
+
+function addTodoToDom(todo) {
+  const node = document.createElement("li");
+  const text = document.createTextNode(todo.name);
+  node.appendChild(text);
+
+  // Adjust the style depending on the status
+  node.style.textDecoration = todo.complete ? "line-through" : "none";
+  node.addEventListener("click", () => {
+    store.dispatch(toggleTodoAction(todo.id));
+  });
+
+  document.getElementById("todos").appendChild(node);
+}
+
+function addGoalToDOM(goal) {
+  const node = document.createElement("li");
+  const text = document.createTextNode(goal.name);
+  node.appendChild(text);
+
+  document.getElementById("goals").appendChild(node);
+}
+
 /*
 store.dispatch(
   addTodoAction({
